c7f8031eb0c9c35b9cb18fb4a3586383b2f79568
5-23 dev <yhanbyeol6@gmail.com>
2023-08-07 18:50:37 +0900

    feat: make #name function

--- better/src/lib.rs
+++ better/src/lib.rs
@@ -8,18 +8,19 @@ pub fn new(_: TokenStream, item: TokenStream) -> TokenStream {
     let item_string = item.to_string();
     
     let input = parse_macro_input!(item as ItemImpl);
-    let name = item_string.split(" ").nth(1).unwrap();
+    let name = format_ident!("{}", item_string.split(" ").nth(1).unwrap());
     // impl Asdf { fn new(a : isize, b : isize) -> Self { Self { a : a, b } } }
     // let reg = Regex::new(&format!("fn new\\((?<a>.*)\\) -> (Self)?({name})?")).unwrap();
     // let attr = &reg.captures(&item_string).unwrap()["a"];
     // let code = &reg.captures(&item_string).unwrap()["c"];
     
-    dbg!(match input.items.get(0).unwrap() {
-        _ => {},
-    });
-
+    // dbg!(match input.items.get(0).unwrap() {
+    //     _ => {},
+    // });
+    println!("name: {name:?}");
     let f = quote!(
-        fn name () {
+        fn #name() -> &'static str {
+            "hello world"
         }
     );
     TokenStream::from(f)
--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,10 @@ impl Asdf {
     fn new(a: isize, b: isize) -> Self{
         Self { a: a, b }
     }
-    fn bb(a: isize, b: isize) -> Self{
-        Self { a: a, b }
-    }
 }
 
 
-
 fn main() {
-    // let a = Asdf{a: 1, };
-    // println!("{:?}",a.a);
+    let a = Asdf();
+    println!("{:?}",a);
 }
